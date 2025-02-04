3523eb95e7af73584498d7b8c2f8a48771a5b7d6
SeregaMechanik <s-a.a3@mail.ru>
2025-02-04 14:27:11 +0300

    Initial commit

--- src/main/java/org/example/Task3_ExclusiveTime.java
+++ src/main/java/org/example/Task3_ExclusiveTime.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Task3_ExclusiveTime {
+    public static int[] exclusiveFunctionsTime(ArrayList<String> execLog, int n)
+    {
+        if (n == 0 || execLog.isEmpty()) {
+            return new int[]{};
+        }
+        int[] result = new int[n];
+        int lastActionStamp = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (String s : execLog) {
+            int funcId = Integer.parseInt(s.substring(0, s.indexOf(':')));
+            String actionType = s.substring(s.indexOf(':') + 1, s.lastIndexOf(':'));
+            int timestamp = Integer.parseInt(s.substring(s.lastIndexOf(':') + 1));
+            if (!stack.isEmpty()) {
+                result[stack.peek()] += timestamp - lastActionStamp;
+            }
+            lastActionStamp = timestamp;
+            if (actionType.equals("start")) {
+                stack.push(funcId);
+            } else {
+                stack.pop();
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/org/example/Task3_ExclusiveTimeTest.java
+++ src/test/java/org/example/Task3_ExclusiveTimeTest.java
@@ -1,12 +1,58 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.*;
 public class Task3_ExclusiveTimeTest {
 
     @Test
     public void exclusiveFunctionsTime() {
+        ArrayList<String> list = new ArrayList<String>() { };
+        list.add("0:start:0");
+        list.add("1:start:5");
+        list.add("2:start:8");
+        list.add("3:start:12");
+        list.add("4:start:15");
+        list.add("5:start:19");
+        list.add("5:end:22");
+        list.add("4:end:24");
+        list.add("3:end:27");
+        list.add("2:end:32");
+        list.add("1:end:35");
+        list.add("0:end:36");
+        int[] result = Task3_ExclusiveTime.exclusiveFunctionsTime(list, 6);
+        Assertions.assertArrayEquals(new int[]{6, 6, 9, 6, 6, 3}, result);
     }
+
+    @Test
+    public void exclusiveFunctionsTime_OneFunction() {
+        ArrayList<String> list = new ArrayList<String>() { };
+        list.add("0:start:0");
+        list.add("0:start:2");
+        list.add("0:end:5");
+        list.add("0:start:6");
+        list.add("0:end:6");
+        list.add("0:end:7");
+        int[] result = Task3_ExclusiveTime.exclusiveFunctionsTime(list, 1);
+        Assertions.assertArrayEquals(new int[]{7}, result);
+    }
+
+    @Test
+    public void exclusiveFunctionsTime_ZeroTime() {
+        ArrayList<String> list = new ArrayList<String>() { };
+        list.add("0:start:0");
+        list.add("0:end:0");
+        list.add("1:start:1");
+        list.add("1:end:1");
+        list.add("2:start:2");
+        list.add("2:end:2");
+        list.add("2:start:3");
+        list.add("2:end:3");
+        int[] result = Task3_ExclusiveTime.exclusiveFunctionsTime(list, 3);
+        Assertions.assertArrayEquals(new int[]{0, 0, 0}, result);
+    }
+
 }
